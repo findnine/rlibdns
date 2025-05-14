f12847b4b42be8f6f960be2ed8a62da4df0a6eba
DrBrad <brad@bradeagle.com>
2025-05-13 21:41:57 -0600

    Response code update

--- src/messages/inter/response_codes.rs
+++ src/messages/inter/response_codes.rs
@@ -3,17 +3,32 @@ use std::io;
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub enum ResponseCodes {
     NoError,
-    FormatError,
-    ServerFailure,
-    NameError,
-    NotImplemented,
-    Refused
+    FormErr,
+    ServFail,
+    NxDomain,
+    NoTimp,
+    Refused,
+    YxDomain,
+    XrrSet,
+    NotAuth,
+    NotZone
 }
 
 impl ResponseCodes {
 
     pub fn from_code(code: u8) -> io::Result<Self> {
-        for c in [Self::NoError, Self::FormatError, Self::ServerFailure, Self::NameError, Self::NotImplemented, Self::Refused] {
+        for c in [
+            Self::NoError,
+            Self::FormErr,
+            Self::ServFail,
+            Self::NxDomain,
+            Self::NoTimp,
+            Self::Refused,
+            Self::YxDomain,
+            Self::XrrSet,
+            Self::NotAuth,
+            Self::NotZone
+        ] {
             if c.get_code() == code {
                 return Ok(c);
             }
@@ -25,22 +40,30 @@ impl ResponseCodes {
     pub fn get_code(&self) -> u8 {
         match self {
             Self::NoError => 0,
-            Self::FormatError => 1,
-            Self::ServerFailure => 2,
-            Self::NameError => 3,
-            Self::NotImplemented => 4,
-            Self::Refused => 5
+            Self::FormErr => 1,
+            Self::ServFail => 2,
+            Self::NxDomain => 3,
+            Self::NoTimp => 4,
+            Self::Refused => 5,
+            Self::YxDomain => 6,
+            Self::XrrSet => 6,
+            Self::NotAuth => 8,
+            Self::NotZone => 9
         }
     }
 
     pub fn to_string(&self) -> String {
         match self {
             Self::NoError => "No Error",
-            Self::FormatError => "Format Error",
-            Self::ServerFailure => "Server Failure",
-            Self::NameError => "Name Error",
-            Self::NotImplemented => "Not Implemented",
-            Self::Refused => "Refused"
+            Self::FormErr => "Format Error",
+            Self::ServFail => "Server Failure",
+            Self::NxDomain => "Domain Not Found",
+            Self::NoTimp => "Not Implemented",
+            Self::Refused => "Refused",
+            Self::YxDomain => "Name Should Not Exist",
+            Self::XrrSet => "RRset Should Not Exist",
+            Self::NotAuth => "Not Authoritative",
+            Self::NotZone => "Name Not In Zone"
         }.to_string()
     }
 }
