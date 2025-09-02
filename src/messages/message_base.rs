529e313c07a69ad056ee2819d2f788c510e9a222
DrBrad <brad@bradeagle.com>
2025-09-02 12:41:06 -0600

    Dynamic records

--- src/messages/message_base.rs
+++ src/messages/message_base.rs
@@ -44,9 +44,10 @@ pub struct MessageBase {
     origin: Option<SocketAddr>,
     destination: Option<SocketAddr>,
     queries: Vec<DnsQuery>,
-    answers: Vec<(String, Box<dyn RecordBase>)>, //WE HAVE TO SWITCH FROM ORDERED_MAP TO VEC FOR RFC 5936
-    authority_records: Vec<(String, Box<dyn RecordBase>)>,
-    additional_records: Vec<(String, Box<dyn RecordBase>)>
+    records: [Vec<(String, Box<dyn RecordBase>)>; 3]
+    //answers: Vec<(String, Box<dyn RecordBase>)>, //WE HAVE TO SWITCH FROM ORDERED_MAP TO VEC FOR RFC 5936
+    //authority_records: Vec<(String, Box<dyn RecordBase>)>,
+    //additional_records: Vec<(String, Box<dyn RecordBase>)>
 }
 
 impl Default for MessageBase {
@@ -66,9 +67,10 @@ impl Default for MessageBase {
             origin: None,
             destination: None,
             queries: Vec::new(),
-            answers: Vec::new(),
-            authority_records: Vec::new(),
-            additional_records: Vec::new()
+            records: std::array::from_fn(|_| Vec::<(String, Box<dyn RecordBase>)>::new())
+            //answers: Vec::new(),
+            //authority_records: Vec::new(),
+            //additional_records: Vec::new()
         }
     }
 }
@@ -99,9 +101,6 @@ impl MessageBase {
         let response_code = ResponseCodes::from_code((flags & 0x000F) as u8).ok_or(io::Error::from(io::ErrorKind::InvalidData))?;
 
         let qd_count = u16::from_be_bytes([buf[4], buf[5]]);
-        let an_count = u16::from_be_bytes([buf[6], buf[7]]);
-        let ns_count = u16::from_be_bytes([buf[8], buf[9]]);
-        let ar_count = u16::from_be_bytes([buf[10], buf[11]]);
 
         let mut queries = Vec::new();
         let mut off = DNS_HEADER_LEN;
@@ -110,9 +109,11 @@ impl MessageBase {
             queries.push(DnsQuery::from_bytes(buf, &mut off));
         }
 
-        let answers = records_from_bytes(buf, &mut off, an_count);
-        let authority_records = records_from_bytes(buf, &mut off, ns_count);
-        let additional_records = records_from_bytes(buf, &mut off, ar_count);
+        let records = [
+            records_from_bytes(buf, &mut off, u16::from_be_bytes([buf[6], buf[7]])),
+            records_from_bytes(buf, &mut off, u16::from_be_bytes([buf[8], buf[9]])),
+            records_from_bytes(buf, &mut off, u16::from_be_bytes([buf[10], buf[11]]))
+        ];
 
         Ok(Self {
             id,
@@ -128,9 +129,7 @@ impl MessageBase {
             origin: None,
             destination: None,
             queries,
-            answers,
-            authority_records,
-            additional_records
+            records
         })
     }
 
@@ -158,27 +157,17 @@ impl MessageBase {
         }
 
         if !truncated {
-            let (records, i, t) = records_to_bytes(off, &self.answers, &mut label_map, max_payload_len);
-            buf.extend_from_slice(&records);
-            truncated = t;
+            for i in 0..3 {
+                let (records, count, t) = records_to_bytes(off, &self.records[i], &mut label_map, max_payload_len);
+                buf.extend_from_slice(&records);
 
-            buf.splice(6..8, i.to_be_bytes());
-        }
+                buf.splice(i*2+6..i*2+8, count.to_be_bytes());
 
-        if !truncated {
-            let (records, i, t) = records_to_bytes(off, &self.authority_records, &mut label_map, max_payload_len);
-            buf.extend_from_slice(&records);
-            truncated = t;
-
-            buf.splice(8..10, i.to_be_bytes());
-        }
-
-        if !truncated {
-            let (records, i, t) = records_to_bytes(off, &self.additional_records, &mut label_map, max_payload_len);
-            buf.extend_from_slice(&records);
-            truncated = t;
-
-            buf.splice(10..12, i.to_be_bytes());
+                if t {
+                    truncated = t;
+                    break;
+                }
+            }
         }
 
         let flags = (if self.qr { 0x8000 } else { 0 }) |  // QR bit
@@ -302,63 +291,63 @@ impl MessageBase {
     }
 
     pub fn has_answers(&self) -> bool {
-        !self.answers.is_empty()
+        !self.records[0].is_empty()
     }
 
     pub fn add_answer(&mut self, query: &str, record: Box<dyn RecordBase>) {
-        self.answers.push((query.to_string(), record));
+        self.records[0].push((query.to_string(), record));
     }
 
     pub fn get_answers(&self) -> &Vec<(String, Box<dyn RecordBase>)> {
-        &self.answers
+        &self.records[0]
     }
 
     pub fn get_answers_mut(&mut self) -> &mut Vec<(String, Box<dyn RecordBase>)> {
-        &mut self.answers
+        &mut self.records[0]
     }
 
     pub fn total_answers(&self) -> usize {
-        self.answers.len()
+        self.records[0].len()
     }
 
     pub fn has_authority_records(&self) -> bool {
-        !self.authority_records.is_empty()
+        !self.records[1].is_empty()
     }
 
     pub fn add_authority_record(&mut self, query: &str, record: Box<dyn RecordBase>) {
-        self.authority_records.push((query.to_string(), record));
+        self.records[1].push((query.to_string(), record));
     }
 
     pub fn get_authority_records(&self) -> &Vec<(String, Box<dyn RecordBase>)> {
-        &self.authority_records
+        &self.records[1]
     }
 
     pub fn get_authority_records_mut(&mut self) -> &mut Vec<(String, Box<dyn RecordBase>)> {
-        &mut self.authority_records
+        &mut self.records[1]
     }
 
     pub fn total_authority_records(&self) -> usize {
-        self.authority_records.len()
+        self.records[1].len()
     }
 
     pub fn has_additional_records(&self) -> bool {
-        !self.additional_records.is_empty()
+        !self.records[2].is_empty()
     }
 
     pub fn add_additional_record(&mut self, query: &str, record: Box<dyn RecordBase>) {
-        self.additional_records.push((query.to_string(), record));
+        self.records[2].push((query.to_string(), record));
     }
 
     pub fn get_additional_records(&self) -> &Vec<(String, Box<dyn RecordBase>)> {
-        &self.additional_records
+        &self.records[2]
     }
 
     pub fn get_additional_records_mut(&mut self) -> &mut Vec<(String, Box<dyn RecordBase>)> {
-        &mut self.additional_records
+        &mut self.records[2]
     }
 
     pub fn total_additional_records(&self) -> usize {
-        self.additional_records.len()
+        self.records[2].len()
     }
 }
 
@@ -380,9 +369,9 @@ impl fmt::Display for MessageBase {
         writeln!(f, ";; flags: {}; QUERY: {}, ANSWER: {}, AUTHORITY: {}, ADDITIONAL: {}",
                 flags.join(" "),
                 self.queries.len(),
-                self.answers.len(),
-                self.authority_records.len(),
-                self.additional_records.len())?;
+                self.records[0].len(),
+                self.records[1].len(),
+                self.records[2].len())?;
 
         /*
         if let Some(r) = self.additional_records.get(&String::new()) {
@@ -400,23 +389,23 @@ impl fmt::Display for MessageBase {
             writeln!(f, ";{}", q)?;
         }
 
-        if !self.answers.is_empty() {
+        if !self.records[0].is_empty() {
             writeln!(f, "\r\n;; ANSWER SECTION:")?;
-            for (q, r) in self.answers.iter() {
+            for (q, r) in self.records[0].iter() {
                 writeln!(f, "{:<24}{}", format!("{}.", q), r)?;
             }
         }
 
-        if !self.authority_records.is_empty() {
+        if !self.records[1].is_empty() {
             writeln!(f, "\r\n;; AUTHORITATIVE SECTION:")?;
-            for (q, r) in self.authority_records.iter() {
+            for (q, r) in self.records[1].iter() {
                 writeln!(f, "{:<24}{}", format!("{}.", q), r)?;
             }
         }
 
-        if !self.additional_records.is_empty() {
+        if !self.records[2].is_empty() {
             writeln!(f, "\r\n;; ADDITIONAL SECTION:")?;
-            for (q, r) in self.additional_records.iter() {
+            for (q, r) in self.records[2].iter() {
                 if !q.eq("") && !r.get_type().eq(&RRTypes::Opt) {
                     writeln!(f, "{:<24}{}", format!("{}.", q), r)?;
                 }
@@ -438,7 +427,7 @@ impl<'a> Iterator for WireIter<'a> {
     type Item = Vec<u8>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.position >= self.message.answers.len() + self.message.additional_records.len() + self.message.authority_records.len() {
+        if self.position >= self.message.records[0].len() + self.message.records[1].len() + self.message.records[2].len() {
             return None;
         }
 
@@ -476,9 +465,9 @@ impl<'a> Iterator for WireIter<'a> {
             off += q.len();
         }
 
-        let total = self.message.answers.len();
+        let total = self.message.records[0].len();
         if !truncated && self.position < total {
-            let (records, i, t) = records_to_bytes(off, &self.message.answers[self.position..], &mut label_map, self.max_payload_len);
+            let (records, i, t) = records_to_bytes(off, &self.message.records[0][self.position..], &mut label_map, self.max_payload_len);
             buf.extend_from_slice(&records);
             truncated = t;
             self.position += i as usize;
@@ -487,9 +476,9 @@ impl<'a> Iterator for WireIter<'a> {
         }
 
         let mut x = total;
-        let total = self.message.authority_records.len();
+        let total = self.message.records[1].len();
         if !truncated && self.position < x + total {
-            let (records, i, t) = records_to_bytes(off, &self.message.authority_records[self.position - x..], &mut label_map, self.max_payload_len);
+            let (records, i, t) = records_to_bytes(off, &self.message.records[1][self.position - x..], &mut label_map, self.max_payload_len);
             buf.extend_from_slice(&records);
             truncated = t;
             self.position += i as usize;
@@ -498,9 +487,9 @@ impl<'a> Iterator for WireIter<'a> {
         }
 
         x += total;
-        let total = self.message.additional_records.len();
+        let total = self.message.records[2].len();
         if !truncated && self.position < x + total {
-            let (records, i, t) = records_to_bytes(off, &self.message.additional_records[self.position - x..], &mut label_map, self.max_payload_len);
+            let (records, i, t) = records_to_bytes(off, &self.message.records[2][self.position - x..], &mut label_map, self.max_payload_len);
             buf.extend_from_slice(&records);
             self.position += i as usize;
 
