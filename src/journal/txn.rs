134bb6ede1fa98299adf65025de21abb33815cfb
DrBrad <brad@bradeagle.com>
2025-09-03 02:35:50 -0600

    TXN journaling

--- src/journal/txn.rs
+++ src/journal/txn.rs
@@ -5,7 +5,7 @@ use crate::records::inter::record_base::RecordBase;
 pub struct Txn {
     pub serial_0: u32,
     pub serial_1: u32,
-    pub buckets: [Vec<(String, Box<dyn RecordBase>)>; 2]
+    pub records: [Vec<(String, Box<dyn RecordBase>)>; 2]
 }
 
 impl Txn {
@@ -14,11 +14,11 @@ impl Txn {
         Self {
             serial_0,
             serial_1,
-            buckets: Default::default()
+            records: Default::default()
         }
     }
 
     pub fn add_record(&mut self, op_code: TxnOpCodes, rec: (String, Box<dyn RecordBase>)) {
-        self.buckets[op_code as usize].push(rec)
+        self.records[op_code as usize].push(rec)
     }
 }
