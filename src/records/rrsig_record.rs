90f6505194b7e57db262ab69902d61549d5dd4bc
DrBrad <brad@bradeagle.com>
2025-10-02 05:52:10 -0600

    RRSig unit test

--- src/records/rrsig_record.rs
+++ src/records/rrsig_record.rs
@@ -2,7 +2,6 @@ use std::any::Any;
 use std::collections::HashMap;
 use std::fmt;
 use std::fmt::Formatter;
-use crate::messages::inter::rr_classes::RRClasses;
 use crate::messages::inter::rr_types::RRTypes;
 use crate::records::inter::record_base::{RecordBase, RecordError};
 use crate::utils::fqdn_utils::{pack_fqdn, unpack_fqdn};
@@ -11,8 +10,6 @@ use crate::utils::time_utils::TimeUtils;
 
 #[derive(Clone, Debug)]
 pub struct RRSigRecord {
-    class: RRClasses,
-    ttl: u32,
     pub(crate) type_covered: RRTypes,
     pub(crate) algorithm: u8,
     pub(crate) labels: u8,
@@ -28,8 +25,6 @@ impl Default for RRSigRecord {
 
     fn default() -> Self {
         Self {
-            class: RRClasses::default(),
-            ttl: 0,
             type_covered: RRTypes::default(),
             algorithm: 0,
             labels: 0,
@@ -48,29 +43,25 @@ impl RecordBase for RRSigRecord {
     fn from_bytes(buf: &[u8], off: usize) -> Result<Self, RecordError> {
         let mut off = off;
 
-        let class = RRClasses::try_from(u16::from_be_bytes([buf[off], buf[off+1]])).unwrap();
-        let ttl = u32::from_be_bytes([buf[off+2], buf[off+3], buf[off+4], buf[off+5]]);
+        let type_covered = RRTypes::try_from(u16::from_be_bytes([buf[off+2], buf[off+3]]))
+            .map_err(|e| RecordError(e.to_string()))?;
 
-        let type_covered = RRTypes::try_from(u16::from_be_bytes([buf[off+8], buf[off+9]])).unwrap();
+        let algorithm = buf[off+4];
+        let labels = buf[off+5];
 
-        let algorithm = buf[off+10];
-        let labels = buf[off+11];
+        let original_ttl = u32::from_be_bytes([buf[off+6], buf[off+7], buf[off+8], buf[off+9]]);
+        let expiration = u32::from_be_bytes([buf[off+10], buf[off+11], buf[off+12], buf[off+13]]);
+        let inception = u32::from_be_bytes([buf[off+14], buf[off+15], buf[off+16], buf[off+17]]);
+        let key_tag = u16::from_be_bytes([buf[off+18], buf[off+19]]);
 
-        let original_ttl = u32::from_be_bytes([buf[off+12], buf[off+13], buf[off+14], buf[off+15]]);
-        let expiration = u32::from_be_bytes([buf[off+16], buf[off+17], buf[off+18], buf[off+19]]);
-        let inception = u32::from_be_bytes([buf[off+20], buf[off+21], buf[off+22], buf[off+23]]);
-        let key_tag = u16::from_be_bytes([buf[off+24], buf[off+25]]);
+        let (signer_name, length) = unpack_fqdn(buf, off+20);
 
-        let (signer_name, length) = unpack_fqdn(buf, off+26);
-
-        let data_length = off+8+u16::from_be_bytes([buf[off+6], buf[off+7]]) as usize;
-        off += length+26;
+        let data_length = off+2+u16::from_be_bytes([buf[off], buf[off+1]]) as usize;
+        off += length+20;
 
         let signature = buf[off..data_length].to_vec();
 
         Ok(Self {
-            class,
-            ttl,
             type_covered,
             algorithm,
             labels,
@@ -86,24 +77,22 @@ impl RecordBase for RRSigRecord {
     fn to_bytes(&self, compression_data: &mut HashMap<String, usize>, off: usize) -> Result<Vec<u8>, RecordError> {
         let mut buf = vec![0u8; 26];
 
-        buf.splice(0..2, self.class.get_code().to_be_bytes());
-        buf.splice(2..6, self.ttl.to_be_bytes());
+        buf.splice(2..4, self.type_covered.get_code().to_be_bytes());
 
-        buf.splice(8..10, self.type_covered.get_code().to_be_bytes());
+        buf[4] = self.algorithm;
+        buf[5] = self.labels;
 
-        buf[10] = self.algorithm;
-        buf[11] = self.labels;
+        buf.splice(6..10, self.original_ttl.to_be_bytes());
+        buf.splice(10..14, self.expiration.to_be_bytes());
+        buf.splice(14..18, self.inception.to_be_bytes());
+        buf.splice(18..20, self.key_tag.to_be_bytes());
 
-        buf.splice(12..16, self.original_ttl.to_be_bytes());
-        buf.splice(16..20, self.expiration.to_be_bytes());
-        buf.splice(20..24, self.inception.to_be_bytes());
-        buf.splice(24..26, self.key_tag.to_be_bytes());
-
-        buf.extend_from_slice(&pack_fqdn(self.signer_name.as_ref().unwrap(), compression_data, off+26, true));
+        buf.extend_from_slice(&pack_fqdn(self.signer_name.as_ref()
+            .ok_or_else(|| RecordError("signer_name param was not set".to_string()))?, compression_data, off+20, true));
 
         buf.extend_from_slice(&self.signature);
 
-        buf.splice(6..8, ((buf.len()-8) as u16).to_be_bytes());
+        buf.splice(0..2, ((buf.len()-2) as u16).to_be_bytes());
 
         Ok(buf)
     }
@@ -131,28 +120,18 @@ impl RecordBase for RRSigRecord {
 
 impl RRSigRecord {
 
-    pub fn new(ttl: u32, class: RRClasses) -> Self {
+    pub fn new(type_covered: RRTypes, algorithm: u8, labels: u8, original_ttl: u32, expiration: u32, inception: u32, key_tag: u16, signer_name: &str, signature: Vec<u8>) -> Self {
         Self {
-            class,
-            ttl,
-            ..Self::default()
+            type_covered,
+            algorithm,
+            labels,
+            original_ttl,
+            expiration,
+            inception,
+            key_tag,
+            signer_name: Some(signer_name.to_string()),
+            signature
         }
-    }
-
-    pub fn set_class(&mut self, class: RRClasses) {
-        self.class = class;
-    }
-
-    pub fn get_class(&self) -> RRClasses {
-        self.class
-    }
-
-    pub fn set_ttl(&mut self, ttl: u32) {
-        self.ttl = ttl;
-    }
-
-    pub fn get_ttl(&self) -> u32 {
-        self.ttl
     }
 
     pub fn set_type_covered(&mut self, type_covered: RRTypes) {
@@ -231,9 +210,7 @@ impl RRSigRecord {
 impl fmt::Display for RRSigRecord {
 
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{:<8}{:<8}{:<8}{} {} {} {} {} {} {} {} {}", self.ttl,
-               self.class.to_string(),
-               self.get_type().to_string(),
+        write!(f, "{:<8}{} {} {} {} {} {} {} {} {}", self.get_type().to_string(),
                self.type_covered.to_string(),
                self.algorithm,
                self.labels,
@@ -244,4 +221,11 @@ impl fmt::Display for RRSigRecord {
                format!("{}.", self.signer_name.as_ref().unwrap()),
                base64::encode(&self.signature))
     }
+}
+
+#[test]
+fn test() {
+    let buf = vec![ 0x0, 0x63, 0x0, 0x1, 0xd, 0x2, 0x0, 0x0, 0x1, 0x2c, 0x68, 0x5e, 0xd8, 0xde, 0x68, 0x5c, 0x19, 0xbe, 0x86, 0xc9, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x5, 0x66, 0x69, 0x6e, 0x64, 0x39, 0x3, 0x6e, 0x65, 0x74, 0x0, 0xb4, 0x43, 0x8e, 0xe4, 0xdc, 0xd0, 0x7c, 0x16, 0x19, 0x8b, 0xbc, 0x9b, 0x25, 0x97, 0x7c, 0xb7, 0xf1, 0xda, 0xa5, 0x7f, 0xe2, 0x51, 0x4f, 0xf0, 0x65, 0x13, 0xf6, 0x11, 0x19, 0xe7, 0xcb, 0x10, 0x86, 0x71, 0xa7, 0xcf, 0x12, 0x85, 0x2a, 0x50, 0x65, 0xa1, 0x22, 0x43, 0x55, 0x93, 0xeb, 0x3, 0x9a, 0x7c, 0x6a, 0x56, 0xdf, 0x21, 0x21, 0x79, 0xcc, 0x19, 0x8b, 0xdd, 0x36, 0x6d, 0xf2, 0x64 ];
+    let record = RRSigRecord::from_bytes(&buf, 0).unwrap();
+    assert_eq!(buf, record.to_bytes(&mut HashMap::new(), 0).unwrap());
 }
