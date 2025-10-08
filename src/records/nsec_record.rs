b08617c75c89f5d675dbc559a394b4bce96fc4a6
DrBrad <brad@bradeagle.com>
2025-10-07 21:59:17 -0600

    NSec record

--- src/records/nsec_record.rs
+++ src/records/nsec_record.rs
@@ -5,26 +5,23 @@ use std::fmt::Formatter;
 use crate::messages::inter::rr_classes::RRClasses;
 use crate::messages::inter::rr_types::RRTypes;
 use crate::records::inter::record_base::{RecordBase, RecordError};
+use crate::utils::base64;
 use crate::utils::fqdn_utils::{pack_fqdn, unpack_fqdn};
+use crate::zone::inter::zone_record::ZoneRecord;
+use crate::zone::zone_reader::{ErrorKind, ZoneReaderError};
 
 #[derive(Clone, Debug)]
 pub struct NSecRecord {
-    class: RRClasses,
-    cache_flush: bool,
-    ttl: u32,
     fqdn: Option<String>,
-    rr_types: Vec<u16>
+    _types: Vec<RRTypes>
 }
 
 impl Default for NSecRecord {
 
     fn default() -> Self {
         Self {
-            class: RRClasses::default(),
-            cache_flush: false,
-            ttl: 0,
             fqdn: None,
-            rr_types: Vec::new()
+            _types: Vec::new()
         }
     }
 }
@@ -34,17 +31,12 @@ impl RecordBase for NSecRecord {
     fn from_bytes(buf: &[u8], off: usize) -> Result<Self, RecordError> {
         let mut off = off;
 
-        let class = u16::from_be_bytes([buf[off], buf[off+1]]);
-        let cache_flush = (class & 0x8000) != 0;
-        let class = RRClasses::try_from(class & 0x7FFF).unwrap();
-        let ttl = u32::from_be_bytes([buf[off+2], buf[off+3], buf[off+4], buf[off+5]]);
+        let (fqdn, length) = unpack_fqdn(buf, off+2);
 
-        let (fqdn, length) = unpack_fqdn(buf, off+8);
-
-        let data_length = off+8+u16::from_be_bytes([buf[off+6], buf[off+7]]) as usize;
+        let data_length = off+2+u16::from_be_bytes([buf[off], buf[off+1]]) as usize;
         off += length+8;
 
-        let mut rr_types = Vec::new();
+        let mut _types = Vec::new();
 
         while off < data_length {
             let window = buf[off];
@@ -59,7 +51,9 @@ impl RecordBase for NSecRecord {
             for (i, &byte) in bitmap.iter().enumerate() {
                 for bit in 0..8 {
                     if byte & (1 << (7 - bit)) != 0 {
-                        rr_types.push((window as u16) * 256 + (i as u16 * 8 + bit as u16));
+                        let _type = RRTypes::try_from((window as u16) * 256 + (i as u16 * 8 + bit as u16))
+                            .map_err(|e| RecordError(e.to_string()))?;
+                        _types.push(_type);
                     }
                 }
             }
@@ -68,32 +62,22 @@ impl RecordBase for NSecRecord {
         }
 
         Ok(Self {
-            class,
-            cache_flush,
-            ttl,
             fqdn: Some(fqdn),
-            rr_types
+            _types
         })
     }
 
     fn to_bytes(&self, compression_data: &mut HashMap<String, usize>, off: usize) -> Result<Vec<u8>, RecordError> {
-        let mut buf = vec![0u8; 8];
+        let mut buf = vec![0u8; 2];
 
-        let mut class = self.class.get_code();
-        if self.cache_flush {
-            class = class | 0x8000;
-        }
-
-        buf.splice(0..2, class.to_be_bytes());
-        buf.splice(2..6, self.ttl.to_be_bytes());
-
-        buf.extend_from_slice(&pack_fqdn(self.fqdn.as_ref().unwrap().as_str(), compression_data, off+10, true));
+        buf.extend_from_slice(&pack_fqdn(self.fqdn.as_ref().unwrap().as_str(), compression_data, off+4, true));
 
         let mut windows: BTreeMap<u8, Vec<u8>> = BTreeMap::new();
 
-        for rr_type in &self.rr_types {
-            let window = (rr_type / 256) as u8;
-            let offset = (rr_type % 256) as usize;
+        for _type in &self._types {
+            let code = _type.get_code();
+            let window = (code / 256) as u8;
+            let offset = (code % 256) as usize;
             let byte_index = offset / 8;
             let bit_index = 7 - (offset % 8);
 
@@ -110,7 +94,7 @@ impl RecordBase for NSecRecord {
             }
         }
 
-        buf.splice(6..8, ((buf.len()-8) as u16).to_be_bytes());
+        buf.splice(0..2, ((buf.len()-2) as u16).to_be_bytes());
 
         Ok(buf)
     }
@@ -138,28 +122,11 @@ impl RecordBase for NSecRecord {
 
 impl NSecRecord {
 
-    pub fn new(ttl: u32, class: RRClasses) -> Self {
+    pub fn new(fqdn: &str, _types: Vec<RRTypes>) -> Self {
         Self {
-            class,
-            ttl,
-            ..Self::default()
+            fqdn: Some(fqdn.to_string()),
+            _types
         }
-    }
-
-    pub fn set_class(&mut self, class: RRClasses) {
-        self.class = class;
-    }
-
-    pub fn get_class(&self) -> RRClasses {
-        self.class
-    }
-
-    pub fn set_ttl(&mut self, ttl: u32) {
-        self.ttl = ttl;
-    }
-
-    pub fn get_ttl(&self) -> u32 {
-        self.ttl
     }
 
     pub fn set_fqdn(&mut self, fqdn: &str) {
@@ -169,11 +136,47 @@ impl NSecRecord {
     pub fn get_fqdn(&self) -> Option<&String> {
         self.fqdn.as_ref()
     }
+
+    pub fn add_type(&mut self, _type: RRTypes) {
+        self._types.push(_type);
+    }
+
+    pub fn get_types(&self) -> &Vec<RRTypes> {
+        self._types.as_ref()
+    }
+
+    pub fn get_types_mut(&mut self) -> &mut Vec<RRTypes> {
+        self._types.as_mut()
+    }
+}
+
+impl ZoneRecord for NSecRecord {
+
+    fn set_data(&mut self, index: usize, value: &str) -> Result<(), ZoneReaderError> {
+        match index {
+            //0 => self.address = Some(value.parse().map_err(|_| ZoneReaderError::new(ErrorKind::FormErr, &format!("unable to parse address param for record type {}", self.get_type())))?),
+            _ => return Err(ZoneReaderError::new(ErrorKind::ExtraRRData, &format!("extra record data found for record type {}", self.get_type())))
+        }
+
+        Ok(())
+    }
+
+    fn upcast(self) -> Box<dyn ZoneRecord> {
+        Box::new(self)
+    }
 }
 
 impl fmt::Display for NSecRecord {
 
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "type {:?}, class {:?}, domain {}", self.get_type(), self.class, self.fqdn.as_ref().unwrap())
+        write!(f, "{:<8}{} ", self.get_type().to_string(),
+               format!("{}.", self.fqdn.as_ref().unwrap_or(&String::new())))
     }
+}
+
+#[test]
+fn test() {
+    let buf = vec![ 0x0, 0x1b, 0x1, 0x0, 0x5, 0x66, 0x69, 0x6e, 0x64, 0x39, 0x3, 0x6e, 0x65, 0x74, 0x0, 0x0, 0x9, 0x62, 0x5, 0x80, 0xc, 0x54, 0xb, 0x8d, 0x1c, 0xc0, 0x1, 0x1, 0xc0 ];
+    let record = NSecRecord::from_bytes(&buf, 0).unwrap();
+    assert_eq!(buf, record.to_bytes(&mut HashMap::new(), 0).unwrap());
 }
