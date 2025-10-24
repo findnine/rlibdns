844327d67a52dba634655e0dfa2f10e159c93d8c
DrBrad <brad@bradeagle.com>
2025-10-24 15:20:07 -0600

    Algorithms

--- src/keyring/key.rs
+++ src/keyring/key.rs
@@ -6,4 +6,30 @@ pub struct Key {
     algorithm: Algorithms
 }
 
+impl Key {
+
+    pub fn new(secret: Vec<u8>, algorithm: Algorithms) -> Self {
+        Self {
+            secret,
+            algorithm
+        }
+    }
+
+    pub fn set_secret(&mut self, secret: &[u8]) {
+        self.secret = secret.to_vec();
+    }
+
+    pub fn secret(&self) -> &[u8] {
+        &self.secret
+    }
+
+    pub fn set_algorithm(&mut self, algorithm: Algorithms) {
+        self.algorithm = algorithm;
+    }
+
+    pub fn algorithm(&self) -> Algorithms {
+        self.algorithm
+    }
+}
+
 
---
+++ src/keyring/keyring.rs
@@ -0,0 +1,14 @@
+
+#[derive(Debug, Clone)]
+pub struct KeyRing {
+
+}
+
+impl KeyRing {
+
+    pub fn new() -> Self {
+        Self {
+
+        }
+    }
+}
